b6da46ae2d89bb63d5cd17e4a5654f4b12d4870a
RilindS <rilindsimnica09@gmailcom>
2025-05-08 21:16:21 +0200

    add ApplicationConfig

--- LogIn-SignUp-Authentication/src/main/java/com/example/LogIn/SignUp/Authentication/security/config/ApplicationConfig.java
+++ LogIn-SignUp-Authentication/src/main/java/com/example/LogIn/SignUp/Authentication/security/config/ApplicationConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -44,12 +45,12 @@ public class ApplicationConfig {
         return authenticationProvider;
     }
 
-//
-//    @Bean
-//    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
-//        log.info("Configured AuthenticationManager");
-//        return configuration.getAuthenticationManager();
-//    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        log.info("Configured AuthenticationManager");
+        return configuration.getAuthenticationManager();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
