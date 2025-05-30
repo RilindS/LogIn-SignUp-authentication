59e967b2203e9ff905d29464e81b191ba0afa71c
RilindS <rilindsimnica09@gmailcom>
2025-05-30 15:44:09 +0200

    fix city

--- LogIn-SignUp-Authentication/src/main/java/com/example/LogIn/SignUp/Authentication/entity/City.java
+++ LogIn-SignUp-Authentication/src/main/java/com/example/LogIn/SignUp/Authentication/entity/City.java
@@ -1,1 +1,18 @@
+package com.example.LogIn.SignUp.Authentication.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "city")
+public class City extends BaseEntity{
+
+    @Column(name = "name")
+    private String name;
+
+}
